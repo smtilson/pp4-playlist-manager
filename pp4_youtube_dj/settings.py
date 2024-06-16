3e2230795b8a1da73f4811a20e2ac7c7315aeebf
seantilson <seantilson@gmail.com>
2024-06-16 15:57:39 +0200

    change to production db

--- pp4_youtube_dj/settings.py
+++ pp4_youtube_dj/settings.py
@@ -28,7 +28,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = os.environ.get("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = [
     "8000-smtilson-pp4playlistman-ym3t1koq57f.ws.codeinstitute-ide.net",
@@ -132,7 +132,7 @@ WSGI_APPLICATION = "pp4_youtube_dj.wsgi.application"
 
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
-
+"""
 DATABASES = {
     "default": {
         "ENGINE": "django.db.backends.sqlite3",
@@ -141,7 +141,7 @@ DATABASES = {
 }
 """
 DATABASES = {"default": dj_database_url.parse(os.environ.get("DATABASE_URL"))}
-"""
+
 # Password validation
 # https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators
 
