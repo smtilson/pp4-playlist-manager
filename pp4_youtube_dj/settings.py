882309f479eafc48f4fade8a6534f473eb5e8aac
seantilson <seantilson@gmail.com>
2024-06-16 15:33:16 +0200

    minor syntax change in settings

--- pp4_youtube_dj/settings.py
+++ pp4_youtube_dj/settings.py
@@ -138,7 +138,8 @@ DATABASES = {
         "ENGINE": "django.db.backends.sqlite3",
         "NAME": BASE_DIR / "db.sqlite3",
     }
-}"""
+}
+"""
 DATABASES = {"default": dj_database_url.parse(os.environ.get("DATABASE_URL"))}
 """
 # Password validation
