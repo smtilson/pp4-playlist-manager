20e6993b29d5229060171710cc1941f4dbeb0183
seantilson <seantilson@gmail.com>
2024-06-22 13:31:49 +0200

    modify error_handler to check first digits

--- errors/views.py
+++ errors/views.py
@@ -10,12 +10,13 @@ from .models import RequestReport
 def error_handler(request, response):
     user = make_user(request)
     status, msg, msg_type = RequestReport.process(response)
-    if status in {200, 302}:
+    status = str(status)[0]
+    if status in {'2', '3'}:
         return response
-    elif status == 404:
+    elif status == '404':
         messages.add_message(request, msg_type, msg)
         return render(request, "404.html")
-    elif status not in {200, 302}:
+    else:
         messages.add_message(request, msg_type, msg)
     if user.is_authenticated:
         return HttpResponseRedirect(reverse("profile"))
